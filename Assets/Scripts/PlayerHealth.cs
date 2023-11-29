7915e4f31d80a0257c43d98d747acb8ae503a590
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-11-29 11:13:37 +0200

    An attempt at making attacks

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,12 +3,15 @@ using FishNet.Broadcast;
 using FishNet.Connection;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : NetworkBehaviour
 {
     [SyncVar]
     public int health = 100;
+    [SerializeField]
+    private TextMeshProUGUI _healthText;
 
     public override void OnStartClient()
     {
@@ -19,22 +22,26 @@ public class PlayerHealth : NetworkBehaviour
         }
     }
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            AdjustHealth(-10);
-        }
+        _healthText = GameObject.Find("HealthText").GetComponent<TextMeshProUGUI>();
+        _healthText.text = (health / 10).ToString();
     }
 
     public void AdjustHealth(int diff)
     {
         UpdateHealth(this, diff);
+        _healthText.text = health.ToString();
     }
 
     [ServerRpc]
     private void UpdateHealth(PlayerHealth script, int diff)
     {
         script.health += diff;
+    }
+
+    public void Die()
+    {
+
     }
 }
